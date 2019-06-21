1b529e7f6ba67e5252fcf38c3886d19b4263629a
Christine <you@example.com>
2019-06-21 11:07:05 +0300

    creates search user function

--- instagram/views.py
+++ instagram/views.py
@@ -11,6 +11,17 @@ def home(request):
     return render(request, 'index.html',{"images":images, "users":users})
 
 
+def get_search(request):
+    
+    if 'searchUser' in request.GET and request.GET["searchUser"]:
+        search_word = request.GET.get("searchUser")
+        searched_user = User.search_by_username(search_word)
+        message = f"{search_word}"
 
+        return render(request, 'index.html',{"message":message,"all_users": searched_user})
+
+    else:
+        message = "You haven't searched for anyone"
+        return render(request, 'index.html',{"message":message})
 
 
