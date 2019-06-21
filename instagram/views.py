ebe02667df42036b414ff9afcfd53050b18545a9
Christine <you@example.com>
2019-06-21 10:49:36 +0300

    creates home function

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Image,Profile
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+@login_required
+def home(request):
+    images = Image.objects.all()
+    users = User.objects.all()
+    return render(request, 'index.html',{"images":images, "users":users})
+
+
+
+
+
